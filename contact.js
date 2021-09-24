073b1a92ede5507018f616ae332862874461d6b5
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-09-24 23:55:37 +0300

    add and get contact

--- contact.js
+++ contact.js
@@ -1,21 +1,34 @@
-const fs = require("fs/promises");
-const path = require("path");
-const crypto = require("crypto");
+const fs = require('fs/promises');
+const path = require('path');
+const crypto = require('crypto');
 
-const contactsPath = path.join(__dirname, "db", "contacts.json")
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
 async function listContacts() {
-  const contacts = await fs.readFile(  contactsPath,"utf-8");
+  const contacts = await fs.readFile(contactsPath, 'utf-8');
   const result = JSON.parse(contacts);
-  
+
   return result;
 }
 
-function getContactById(contactId) {}
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const [result] = contacts.filter(contact =>contact.id===contactId);
+  return result;
+}
 
 function removeContact(contactId) {}
 
-function addContact(name, email, phone) {}
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: crypto.randomUUID(), name, email, phone };
+
+  contacts.push(newContact);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return newContact;
+}
 
 module.exports = {
   listContacts,
--- index.js
+++ index.js
@@ -1,42 +1,60 @@
-const { Command } = require("commander");
-const chalk = require("chalk");
-const { listContacts } = require("./contact");
+const { Command } = require('commander');
+const chalk = require('chalk');
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require('./contact');
 const program = new Command();
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
 
 program.parse(process.argv);
 
 const argv = program.opts();
 
-// TODO: рефакторить
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
-    case "list":
+    case 'list':
       listContacts()
-        .then((contacts) => console.table(contacts))
+        .then(contacts => console.table(contacts))
         .catch(console.error);
 
       break;
 
-    case "get":
-      // ... id
+    case 'get':
+      getContactById(id)
+        .then(contact => {
+          if (contact) {
+            console.log(chalk.green('Contact is found'));
+            console.log(contact);
+          } else {
+            console.log(chalk.red('Contact not found'));
+          }
+        })
+        .catch(console.error);
       break;
 
-    case "add":
-      // ... name email phone
+    case 'add':
+      addContact(name, email, phone)
+        .then(contact => {
+          console.log(chalk.green('Add new contact'));
+          console.log(contact);
+        })
+        .catch(console.error);
       break;
 
-    case "remove":
+    case 'remove':
       // ... id
       break;
 
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.warn(chalk.red('Unknown action type!'));
   }
 }
 
